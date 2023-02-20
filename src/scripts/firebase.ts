0a8b66e256d3cd72d36010c456854a8d368c0a1e
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-21 01:37:34 +0200

    Firebase config

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDQWxYPzGewJ8OebLV5kf70bqzW8wruAVo",
-	appId: "1:1035330291676:web:575f3933865aef74ac3119",
-	authDomain: "playform-web.firebaseapp.com",
-	databaseURL: "https://playform-web.firebaseio.com",
-	measurementId: "G-D63244WLE8",
-	messagingSenderId: "1035330291676",
-	projectId: "playform-web",
-	storageBucket: "playform-web.appspot.com",
+	apiKey: "AIzaSyAkevZLi2u7BNRBxvnwYTTgRbpbQYWjGL8",
+	appId: "1:541150662775:web:574599325594e05ae7f672",
+	authDomain: "playform-cloud.firebaseapp.com",
+	measurementId: "G-8FCDP89WYD",
+	messagingSenderId: "541150662775",
+	projectId: "playform-cloud",
+	storageBucket: "playform-cloud.appspot.com",
 };
 
 initializeApp(firebaseConfig);

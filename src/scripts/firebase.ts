1e354034356dc7647b66cd2eb8f3221a83a05703
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:06:20 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAkevZLi2u7BNRBxvnwYTTgRbpbQYWjGL8",
-	appId: "1:541150662775:web:574599325594e05ae7f672",
 	authDomain: "playform-cloud.firebaseapp.com",
-	measurementId: "G-8FCDP89WYD",
-	messagingSenderId: "541150662775",
 	projectId: "playform-cloud",
 	storageBucket: "playform-cloud.appspot.com",
+	messagingSenderId: "541150662775",
+	appId: "1:541150662775:web:574599325594e05ae7f672",
+	measurementId: "G-RFLGQF2GPJ",
 };
 
 initializeApp(firebaseConfig);

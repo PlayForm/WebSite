dc9ed1047c96f8b12ad469af31aa2f9c71694011
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-08 23:13:07 +0300

    squash!

--- Source/Script/Visualization.ts
+++
@@ -1,8 +0,0 @@
-const visualization =
-	document.querySelector<HTMLVideoElement>("#visualization");
-
-if (visualization) {
-	visualization.srcObject = await navigator.mediaDevices.getUserMedia({ video: true });
-}
-
-export { };

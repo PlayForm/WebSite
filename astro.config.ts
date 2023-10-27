f9ccf68bdd9d6e8c9fe5dabfe66ba90f74880a6b
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-27 04:32:10 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -3,22 +3,19 @@ export default (await import("astro/config")).defineConfig({
 	publicDir: "./Public",
 	outDir: "./Target",
 	site: "https://playform.cloud",
-	compressHTML: true,
+	compressHTML: false,
 	experimental: {
 		devOverlay: true,
 	},
 	integrations: [
-		// import.meta.env.MODE === "production"
-		// 	? (await import("astrojs-service-worker")).default()
-		// 	: null,
-		// (await import("@astrojs/sitemap")).default(),
-		// (await import("astro-critters")).default({ Logger: 1 }),
-		// (await import("@astrojs/prefetch")).default(),
-		// (await import("astro-rome")).default({ Logger: 1 }),
-		// (await import("astro-compress")).default({
-		// 	Logger: 2,
-		// 	Path: ["./.github"],
-		// }),
+		import.meta.env.MODE === "production"
+			? (await import("astrojs-service-worker")).default()
+			: null,
+		(await import("@astrojs/sitemap")).default(),
+		(await import("astro-critters")).default({ Logger: 1 }),
+		(await import("@astrojs/prefetch")).default(),
+		(await import("astro-rome")).default({ Logger: 1 }),
+		(await import("astro-compress")).default({ Logger: 1 }),
 	],
 	vite: {
 		build: {

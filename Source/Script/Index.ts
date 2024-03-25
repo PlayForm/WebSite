f4f100d2c3b9ded56f8067bc2cb51d49bcf04458
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-25 14:38:24 +0200

    squash!

--- Source/Script/Index.ts
+++ Source/Script/Index.ts
@@ -1,24 +1,26 @@
 function debounce(_Function, Delay) {
 	let Timer;
 
-	return function (...Argumnents) {
+	return function (...Argument) {
 		if (Timer) {
 			clearTimeout(Timer);
 		}
 		Timer = setTimeout(() => {
-			_Function.apply(this, Argumnents);
+			_Function.apply(this, Argument);
 			Timer = null;
 		}, Delay);
 	};
 }
 
-document
-  .querySelector<HTMLInputElement>("#mce-EMAIL")
-  ?.addEventListener(
-    "input",
-    debounce((Event) => {
-      /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Event.target.value)
-        ? document.querySelector<HTMLFormElement>('[name="mc-embedded-subscribe-form"]')?.submit()
-        : null;
-    }, 5000)
-  );
+document.querySelector<HTMLInputElement>("#mce-EMAIL")?.addEventListener(
+	"input",
+	debounce((Event) => {
+		/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Event.target.value)
+			? document
+					.querySelector<HTMLFormElement>(
+						'[name="mc-embedded-subscribe-form"]',
+					)
+					?.submit()
+			: null;
+	}, 5000),
+);

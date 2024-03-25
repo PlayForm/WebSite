ba56af95650a8ad7c4d6493c05292f8ee7466a11
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-25 04:00:14 +0200

    squash!

---
+++ Source/Script/Index.ts
@@ -0,0 +1,24 @@
+function debounce(_Function, Delay) {
+	let Timer;
+
+	return function (...Argumnents) {
+		if (Timer) {
+			clearTimeout(Timer);
+		}
+		Timer = setTimeout(() => {
+			_Function.apply(this, Argumnents);
+			Timer = null;
+		}, Delay);
+	};
+}
+
+document
+  .querySelector<HTMLInputElement>("#mce-EMAIL")
+  ?.addEventListener(
+    "input",
+    debounce((Event) => {
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Event.target.value)
+        ? document.querySelector<HTMLFormElement>('[name="mc-embedded-subscribe-form"]')?.submit()
+        : null;
+    }, 7000)
+  );

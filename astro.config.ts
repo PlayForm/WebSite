d134f99645f4c3107ad1dfa6bfa12c167be72ad3
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-29 02:08:45 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -13,13 +13,7 @@ export default (await import("astro/config")).defineConfig({
 		(await import("@playform/inline")).default({ Logger: 1 }),
 		(await import("@astrojs/prefetch")).default(),
 		(await import("@playform/format")).default({ Logger: 1 }),
-		(await import("@playform/compress")).default({
-			Logger: 1,
-			CSS: {
-				// @ts-expect-error
-				lightningcss: false,
-			},
-		}),
+		(await import("@playform/compress")).default({ Logger: 1 }),
 	],
 	experimental: {
 		directRenderScript: true,

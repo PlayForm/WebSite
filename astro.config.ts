8190150f8f2609b4e25e07d53a8e7d41a59ebfb7
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-30 12:00:45 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- astro.config.ts
+++ astro.config.ts
@@ -12,10 +12,7 @@ export default (await import("astro/config")).defineConfig({
 		(await import("@astrojs/sitemap")).default(),
 		(await import("@playform/inline")).default({ Logger: 1 }),
 		(await import("@astrojs/prefetch")).default(),
-		(await import("@playform/format")).default({
-			Logger: 1,
-			Path: ["./Test"],
-		}),
+		(await import("@playform/format")).default({ Logger: 1 }),
 		(await import("@playform/compress")).default({ Logger: 1 }),
 	],
 	experimental: {

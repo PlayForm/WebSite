4161ad49813c36eafa625a8ff49fd8ed267ae1b5
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-21 13:52:24 +0300

    s Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

---
+++ astro.config.ts
@@ -0,0 +1,35 @@
+export default (await import("astro/config")).defineConfig({
+	srcDir: "./Source",
+	publicDir: "./Public",
+	outDir: "./Target",
+	site: "https://playform.cloud",
+	compressHTML: true,
+	prefetch: true,
+	integrations: [
+		// @ts-ignore
+		import.meta.env.MODE === "production"
+			? (await import("astrojs-service-worker")).default()
+			: null,
+		(await import("@astrojs/sitemap")).default(),
+		(await import("@playform/inline")).default({ Logger: 1 }),
+		(await import("@playform/format")).default({ Logger: 1 }),
+		(await import("@playform/compress")).default({ Logger: 1 }),
+	],
+	experimental: {
+		directRenderScript: true,
+		clientPrerender: true,
+		globalRoutePriority: true,
+		serverIslands: true,
+	},
+	vite: {
+		build: {
+			sourcemap: true,
+		},
+		css: {
+			devSourcemap: true,
+			transformer: "lightningcss",
+		},
+	},
+}) as typeof defineConfig;
+
+import type { defineConfig } from "astro/config";
